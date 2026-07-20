172615e557001bc2ea53ca1ff451dd43efb6178d
adamhaitham449 <adamhaitham449@gmail.com>
2026-07-20 11:58:23 +0300

    Fix Railway host

--- server/server.js
+++ server/server.js
@@ -18,10 +18,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "../client")));
 
-app.get("/", (req,res)=>{
-    res.sendFile(
-        path.join(__dirname,"../client/home.html")
-    );
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/login.html"));
 });
 // =============================
 // جميع الغرف
