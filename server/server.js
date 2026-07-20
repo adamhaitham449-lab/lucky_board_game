b8babd6ae2de650c794aa58d51debf52930c551d
adamhaitham449 <adamhaitham449@gmail.com>
2026-07-20 12:14:09 +0300

    Fix main page route

--- server/server.js
+++ server/server.js
@@ -18,6 +18,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "../client")));
 
+app.use(express.static(path.join(__dirname, "../client")));
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/login.html"));
 });
